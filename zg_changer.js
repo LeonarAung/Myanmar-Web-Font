9a737d4848af90bc9271e98f4587c5192a4109f2
Leonar Aung <leonaraung1990@gmail.com>
2017-09-10 13:16:31 +0700

    Update zg_changer.js

--- zg_changer.js
+++ zg_changer.js
@@ -1,1 +1,1 @@
-if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/LeonarAung/MyanmarFont/02f8b62b/zawgyi.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/LeonarAung/MyanmarFont/02f8b62b/zawgyi.css">');
